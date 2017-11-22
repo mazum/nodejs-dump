b43acd682397bcacd366ac6c4af8213f02b6d793
Abhishek Mazumder <mazumder.abhishek@gmail.com>
2017-11-22 17:00:00 +1100

    06-explicit-waits-in-selenium-webdriver-js library.js

---
+++ webdriverjs/06-explicit-waits-in-selenium-webdriver-js/library.js
@@ -0,0 +1,16 @@
+var webdriver = require ('selenium-webdriver'),
+    By = webdriver.By,
+    until = webdriver.until;
+
+var driver = new webdriver.Builder().forBrowser('chrome').build();
+driver.get('http://library-app.firebaseapp.com');
+driver.findElement(By.css('input')).sendKeys('user@email.com');
+driver.findElement(By.css('.btn-lg')).click();
+driver.wait(until.elementLocated(By.css('.alert-success')), 5000).getText().then(function (txt) {
+    console.log("Alert success text is: " + txt);
+});
+
+
+
+driver.sleep(1000);
+driver.quit();
