6daa08cd3092ed93ebb914af834dbd9a6c7fac01
Abhishek Mazumder <mazumder.abhishek@gmail.com>
2017-12-16 00:41:37 +1100

    03-sendkeys-and-click-in-webdriverjs library.js.

---
+++ webdriverjs/03-sendkeys-and-click-in-webdriverjs/library.js
@@ -0,0 +1,17 @@
+var webdriver = require ('selenium-webdriver'),
+    By = webdriver.By,
+    until = webdriver.until;
+
+var driver = new webdriver.Builder().forBrowser('chrome').build();
+driver.get('http://library-app.firebaseapp.com');
+
+driver.findElement(By.css('input')).sendKeys('user@email.com');
+driver.findElement(By.css('.btn-lg')).click();
+//driver.findElement(By.css('.alert-success')).getText().then(function (txt) {
+//    console.log("Alert success text is: " + txt);
+//});
+
+
+
+driver.sleep(1000);
+driver.quit();
