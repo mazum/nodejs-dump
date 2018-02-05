aed51f268fa97ba69c8b4fceda223b2ccc1f7b9c
Abhishek Mazumder <mazumder.abhishek@gmail.com>
2018-02-05 23:48:49 +1100

    16-using-jest-with-webdriverjs utils/fake_data.js

---
+++ webdriverjs/16-using-jest-with-webdriverjs/test/libraryPage.test.js
@@ -0,0 +1,37 @@
+//var { describe, it, after, before} = require('selenium-webdriver/testing');
+var Page = require('../lib/libraries_page');
+var chai = require('chai');
+var chaiAsPromised = require('chai-as-promised');
+var should = chai.should();
+var page;
+chai.use(chaiAsPromised);
+
+
+describe('library app scenarios: Library listings', function () {
+    jest.setTimeout(50000);
+
+    beforeEach(function () {
+        page = new Page();
+        page.driver.manage().window().maximize();
+        page.visit('http://library-app.firebaseapp.com/libraries');
+    });
+
+    afterEach(function () {
+        page.quit();
+    });
+
+    it('Should list libraries', function () {
+        var libraries = page.listLibraries();
+        return libraries.should.eventually.have.length.above(0);
+    });
+
+    it('Should be able to add a new library', function () {
+        var addLibrary = page.addLibrary();
+        return addLibrary.libraryList.should.eventually.contain(addLibrary.libraryName);
+    });
+
+    it('Should be able to sort libraries', function () {
+        var libraries = page.filterLibraries();
+        return libraries.libraryList.should.eventually.contain(libraries.libraryName);
+    });
+});
