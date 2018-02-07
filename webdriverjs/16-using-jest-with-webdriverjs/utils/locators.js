6c1865c5e9881cfcca40be7e4d9824ee7e66d9b8
Abhishek Mazumder <mazumder.abhishek@gmail.com>
2018-02-07 23:50:34 +1100

    16-using-jest-with-webdriverjs utils/locators.js

---
+++ webdriverjs/16-using-jest-with-webdriverjs/utils/locators.js
@@ -0,0 +1,17 @@
+module.exports = {
+    emailInput: 'input',
+    requestInviteBtn: '.btn-lg',
+    alertSuccess: '.alert-success',
+
+    //Library Page
+    libraryContainer: 'body > div.ember-view > div > div:nth-child(6)',
+    libraryItem: '.library-item',
+    //Add new
+    nameInput: 'input[placeholder="The name of the Library"]',
+    addressInput: 'input[placeholder="The address of the Library"]',
+    phoneInput: 'input[placeholder="The phone number of the Library"]',
+    createBtn: '//button[contains(text(),"Create")]',
+
+    //Alphabetical sorting
+    inputFilter: 'input[placeholder="Filter by library name"]'
+}
